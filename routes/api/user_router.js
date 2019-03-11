05602f45cf72760ca0592b4d2c4ac5fc314b52f9
wangxiaowei <wangxiaowei@xiaoxiangyoupin.cn>
2019-03-11 20:05:49 +0800

    api

--- routes/api/user_router.js
+++ routes/api/user_router.js
@@ -1,1 +1,7 @@
 var router = require('koa')()
+var user_controller = require('../../app/controllers/user_controller')
+
+router.get('/getUser', user_controller.getUser)
+router.post('/registerUser', user_controller.registerUser)
+
+module.exports
